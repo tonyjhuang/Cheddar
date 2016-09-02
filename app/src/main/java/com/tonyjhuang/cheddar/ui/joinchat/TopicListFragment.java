c0e25965e04bd03ecd083b85719602d0a38f495a
Tony Huang <tony.huang.jun@gmail.com>
2016-09-02 14:21:30 -0400

    remove topic functionality

--- app/src/main/java/com/tonyjhuang/cheddar/ui/joinchat/TopicListFragment.java
+++ app/src/main/java/com/tonyjhuang/cheddar/ui/joinchat/TopicListFragment.java
@@ -9,21 +9,18 @@ import com.tonyjhuang.cheddar.R;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EFragment;
-import org.androidannotations.annotations.Extra;
+import org.androidannotations.annotations.FragmentArg;
 import org.androidannotations.annotations.ViewById;
 import org.greenrobot.eventbus.EventBus;
 
 import timber.log.Timber;
 
-/**
- * Created by tonyjhuang on 9/1/16.
- */
 @EFragment(R.layout.fragment_topic_list)
 public class TopicListFragment extends Fragment{
     @ViewById(R.id.topic_view)
     RecyclerView topicView;
 
-    @Extra
+    @FragmentArg
     TopicTree topicTree;
 
     @AfterViews
